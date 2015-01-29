39816e5f14c4affe819636635cc20c7b40ba99e9
Minas Anastasi <minasanastasi@Minass-MacBook-Pro.local>
2015-01-29 11:49:42 +0000

    divide

--- divide.c
+++ divide.c
@@ -4,9 +4,7 @@
 //This is the line causing issue in the conflict branch
 >>>>>>> conflict
 
-//This is the second line causing issue in the <*master> branch
 
-//This is the second line causing issue in the <*conflict> branch
 
 int main()
 {
